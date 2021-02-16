5648be531caf107e9f1818306856014acd713c01
orangeztorm <taiwokenny45@gmail.com>
2021-02-16 14:30:03 +0100

    css styling

--- src/components/layouts/SignedInLinks.js
+++ src/components/layouts/SignedInLinks.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom';
 const SignedIn = () => {
     return(
         <ul className="right">
-            <li><NavLink to='/'>New Project</NavLink></li>
+            <li><NavLink to='/create'>New Project</NavLink></li>
             <li><NavLink to='/'>Log Out</NavLink></li>
             <li><NavLink to='/' className='btn btn-floating pinklighten-1'>TK</NavLink></li>
         </ul>
