1b7392726dce26f8da201429c227f761d4f524de
orangeztorm <taiwokenny45@gmail.com>
2021-02-16 14:51:50 +0100

    adding redux

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import {combineReducers} from "redux";
+import projectReducer from "./projectReducer";
+import authReducer from "./authReducer";
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    project: projectReducer
+    }
+);
+
+
+export default rootReducer
