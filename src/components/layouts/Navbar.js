41761591324b0aa37b8d40b253e0fbd48276a262
orangeztorm <taiwokenny45@gmail.com>
2021-02-17 12:48:10 +0100

    adding firebase auth

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import SignedIn from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
+import {connect} from 'react-redux';
 
 const Navbar = () => {
     return(
@@ -15,4 +16,9 @@ const Navbar = () => {
     );
 };
 
-export default Navbar
+const mapStateToProps = (state) => {
+    console.log(state);
+    return (null);
+};
+
+export default connect(mapStateToProps)(Navbar);
